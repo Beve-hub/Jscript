3450c5dc36f8ae3b6f23262d9498488350af0089
Believe <117173080+Beve-hub@users.noreply.github.com>
2023-08-21 21:22:55 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+// let js = "amazing"
+// if (js === "amazing") alert("stage 1 learning!")
+
+// let firstName = "Godfather";
+
+// console.log(firstName + js); 
+
+// let javascript = true;
+//console.log(javascript);
+
+//javascriptIssues = 'YES!'; console.log(typeof javascriptIssues);
+
+
+// mathematical operators
+/*
+const ageJoes = 'joy';
+console.log(ageJoes);
+
+const now = 2023;
+const ageSam = 1977;
+const ageKate = 'nancy';
+console.log(ageSam, ageKate);
+
+console.log(ageKate +' '+ ageJoes );
+*/
+
+const firstName = 'John';
+const lastName = 'doe';
+console.log(firstName + '' + lastName);
+
+console.log(firstName > lastName)
